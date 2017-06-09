aabf085c2ccc28a16ab56241347c4f7cf2e5bcfb
Lukasz Dorau <lukasz.dorau@intel.com>
2017-06-09 08:11:19 +0200

    Add syscall flags for paths

--- src/ebpf_syscalls.h
+++ src/ebpf_syscalls.h
@@ -69,26 +69,46 @@ enum masks_t {
 	/* syscall has fd as sixth arg */
 	EM_fd_6 = 1 << 11,
 
+	/* syscall has path as a first arg */
+	EM_path_1 = 1 << 12,
+	/* syscall has path as second arg */
+	EM_path_2 = 1 << 13,
+	/* syscall has path as third arg */
+	EM_path_3 = 1 << 14,
+	/* syscall has path as fourth arg */
+	EM_path_4 = 1 << 15,
+	/* syscall has path as fifth arg */
+	EM_path_5 = 1 << 16,
+	/* syscall has path as sixth arg */
+	EM_path_6 = 1 << 17,
+
 	/* '*at' type syscall (dirfd + path) */
-	EM_fileat = 1 << 12,
+	EM_fileat = 1 << 18,
 	/* double '*at' type syscall (dirfd + path) */
-	EM_fileat2 = EM_fileat | 1 << 13,
+	EM_fileat2 = EM_fileat | 1 << 19,
 
 	/* syscall does not return */
-	EM_no_ret = 1 << 14,
+	EM_no_ret = 1 << 20,
 	/* syscall returns a file descriptor */
-	EM_rfd = 1 << 15,
+	EM_rfd = 1 << 21,
 	/* syscall returns a PID */
-	EM_rpid = 1 << 16,
+	EM_rpid = 1 << 22,
 
-	/* syscall has fs paths as first and second Args. rename() */
+	/* syscall has strings as first and second args. rename() */
 	EM_str_1_2 = EM_str_1 | EM_str_2,
-	/* syscall has fs paths as first and third Args. linkat() */
+	/* syscall has strings as first and third args. linkat() */
 	EM_str_1_3 = EM_str_1 | EM_str_3,
-	/* syscall has fs paths as second and forth Args. renameat() */
+	/* syscall has strings as second and forth args. renameat() */
 	EM_str_2_4 = EM_str_2 | EM_str_4,
 	/* syscall has strings as 1st, 2nd and 3rd args. mount() */
 	EM_str_1_2_3 = EM_str_1 | EM_str_2 | EM_str_3,
+
+	/* syscall has paths as first and second args. rename() */
+	EM_path_1_2 = EM_path_1 | EM_path_2,
+	/* syscall has fs paths as first and third args. linkat() */
+	EM_path_1_3 = EM_path_1 | EM_path_3,
+	/* syscall has fs paths as second and forth args. renameat() */
+	EM_path_2_4 = EM_path_2 | EM_path_4,
 
 	/* syscall has fd as first and third args. renameat() */
 	EM_fd_1_3 = EM_fd_1 | EM_fd_3,
